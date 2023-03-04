a0b980f13b1a2b946dd8e3fc4fa08539fd7911c9
akmukhi <92871846+akmukhi@users.noreply.github.com>
2023-03-04 09:43:45 -0500

    added the splunk stuff

--- General/Splunk/installSplunkForwarder.sh
+++ General/Splunk/installSplunkForwarder.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-ip = 
+ip = 172.20.241.20
 port = 
 user = 
 passwd = 
 
 cd /opt
 
+echo "Downloading forwarder"
+
+wget -O splunkforwarder-9.0.4-de405f4a7979-Linux-x86_64.tgz "https://download.splunk.com/products/universalforwarder/releases/9.0.4/linux/splunkforwarder-9.0.4-de405f4a7979-Linux-x86_64.tgz"
+tar -xzvf /opt/splunk.tgz
 cd /opt/splunkforwarder/bin
 ./splunk start --accept-license
 
